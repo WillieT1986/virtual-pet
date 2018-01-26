82ce07e0e5d174ffbe66da11f7616fe94e192981
William <WarMad2@Hotmail.com>
2018-01-26 16:58:18 -0500

    updated code: Swapped setters w/ new methods; removed sickness attribute; working on Options

--- src/main/java/virtual/pet/VirtualPet.java
+++ src/main/java/virtual/pet/VirtualPet.java
@@ -4,15 +4,16 @@ public class VirtualPet {
 	private int hunger;
 	private int water;
 	private int waste;
-	boolean tired;
-	boolean bored;
-	boolean sickness;
+	private int tired;
+	private int bored;
 
 	// Constructor
-	public VirtualPet(int hunger, int water, int waste) {
+	public VirtualPet(int hunger, int water, int waste, int tired, int bored) {
 		this.hunger = hunger;
 		this.water = water;
 		this.waste = waste;
+		this.tired = tired;
+		this.bored = bored;
 	}
 
 	// Getter
@@ -28,17 +29,39 @@ public class VirtualPet {
 		return waste;
 	}
 
-	// Setter
-	public void setHunger(int hunger) {
-		this.hunger = hunger;
+	public int getTired() {
+		return tired;
 	}
 
-	public void setWater(int water) {
-		this.water = water;
+	public int getBored() {
+		return bored;
 	}
 
-	public void setWaste(int waste) {
-		this.waste = waste;
+	public void feedPet(int hunger) {
+		this.hunger += 1;
+		this.water -= 1;
+		this.waste += 1;
+		this.tired -= 1;
+	}
+
+	public void sipWater(int water) {
+		this.water += 1;
+		this.waste += 1;
+	}
+
+	public void hasWaste(int waste) {
+		this.waste -= 2;
+		this.tired += 1;
+
+	}
+
+	public void sleepy(int tired) {
+		this.tired -= 1;
+	}
+
+	public void isBored(int bored) {
+		this.bored -= 1;
+		this.tired += 1;
 	}
 
 }
--- src/main/java/virtual/pet/VirtualPetApp.java
+++ src/main/java/virtual/pet/VirtualPetApp.java
@@ -7,20 +7,32 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		VirtualPet myPet = new VirtualPet(10, 10, 10);
+		VirtualPet myPet = new VirtualPet(10, 10, 10, 10, 10);
 
 		System.out.println("Hold the Dog");
-		System.out.println("Hunger: " + myPet.getHunger());
-		System.out.println("Water:  " + myPet.getWater());
-		System.out.println("Waste:  " + myPet.getWaste());
-		System.out.println("Tiredness:  \n");
+		System.out.println("Hunger:    " + myPet.getHunger());
+		System.out.println("Water:     " + myPet.getWater());
+		System.out.println("Waste:     " + myPet.getWaste());
+		System.out.println("Tiredness: " + myPet.getTired());
+		System.out.println("Bored      " + myPet.getBored());
 
-		System.out.println("Which option would you like to choose?");
+		String optionEntered = "";
+
+		System.out.println("\nWhich option would you like to choose?");
 		System.out.println("1. Feed Hold");
 		System.out.println("2. Water Hold");
 		System.out.println("3. Play with Hold");
 		System.out.println("4. Put Hold to sleep");
 		System.out.println("5. Do nothing...");
+		optionEntered = input.nextLine();
+
+		if (optionEntered.equals("1")) {
+			int hunger = 1;
+			myPet.feedPet(hunger);
+			System.out.println("Fed Hold " + hunger + " biscuit.");
+			System.out.println("Feeding Hold a bisucuit took away 1 ");
+		}
+		// if
 
 		input.close();
 	}
